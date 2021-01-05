9fe5c38103fc94a3496eb37e022b2269ef8629a5
Tyler Weinacker <tweinacker57@gmail.com>
2021-01-04 21:19:00 -0500

    Add logout

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 
 Vue.use(VueRouter)
@@ -20,17 +21,14 @@ const routes = [
   },
   {
     path: "/signup",
-    name: "signup",
+    name: "Signup",
     component: Signup
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/logout",
+    name: "Logout",
+    component: Logout
+  },
 ]
 
 const router = new VueRouter({
